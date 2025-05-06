21ae51ea31448510876bfad78a849ce6e3cff8e7
Marlon Salazar <alu0101433943@ull.edu.es>
2025-05-06 10:37:43 +0000

    Consulta de bienes

--- src/index.ts
+++ src/index.ts
@@ -17,13 +17,35 @@ async function main() {
       .catch(err => res.status(400).json(err));
   });
 
-  app.get('/goods', async (_req: Request, res: Response) => {
-    try {
-      const goods = await GoodModel.find().exec();
-      res.json(goods);
-    } catch (err) {
-      res.status(500).json({ message: 'Error al recuperar bienes' });
-    }
+  app.get('/goods', (req: Request, res: Response) => {
+    const filter = req.query.name ? { name: req.query.name.toString() } : {};
+
+    GoodModel.find(filter).exec()
+      .then((goods) => {
+        if (goods.length !== 0) {
+          res.json(goods);
+        } else {
+          res.status(404).send({ message: 'No se encontraron bienes' });
+        }
+      })
+      .catch(() => {
+        res.status(500).send({ message: 'Error al recuperar bienes' });
+      });
+  });
+
+  // Obtener un bien por su ID
+  app.get('/goods/:id', (req: Request, res: Response) => {
+    GoodModel.findById(req.params.id).exec()
+      .then((good) => {
+        if (!good) {
+          res.status(404).send({ message: 'Bien no encontrado' });
+        } else {
+          res.json(good);
+        }
+      })
+      .catch(() => {
+        res.status(500).send({ message: 'Error al recuperar el bien' });
+      });
   });
 
   app.all('/{*splat}', (_, res) => {
