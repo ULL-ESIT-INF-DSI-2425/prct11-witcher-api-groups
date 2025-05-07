2c8a1ca14d6fa97f1aedce8286d9c312b1a5c89e
AlejandroReyes1594 <alu0101552316@ull.edu.es>
2025-05-07 21:22:47 +0000

    reestablecimiento mongoose.ts

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,15 +1,9 @@
 import { connect } from 'mongoose';
 
 export async function connectMongoose() {
-  const uri = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/Lobo-Blanco'; // Valor predeterminado
-
-  if (!uri) {
-    console.error('Error: La variable de entorno MONGODB_URL no está definida y no se proporcionó un valor predeterminado.');
-    process.exit(1); // Salir del proceso si no hay URI
-  }
-
+  const uri = process.env.MONGODB_URL!;
   try {
-    await connect(uri); // Conexión sin opciones adicionales
+    await connect(uri);
     console.log('Connected to MongoDB via Mongoose');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err);
